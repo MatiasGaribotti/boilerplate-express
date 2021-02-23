6601a874ae5fe66f71a8854319cf37b18dca865b
Matías Garibotti <matiasgaribottir@hotmail.com>
2021-02-22 21:54:43 -0300

    express.static() middleware fixed.

--- myApp.js
+++ myApp.js
@@ -8,7 +8,7 @@ console.log("Hello World");
 //});
 
 //Middleware for static stylesheet
-app.use('/public', express.static(__dirname + '/public/style.css'));
+app.use('/public', express.static(__dirname + '/public'));
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
